74dfe8149e4b8334a2493863a254ebc976517b81
tusharcybo <tusharcybo@gmail.com>
2025-03-10 02:54:03 +0530

    Added first POST request handler for Catalog API microservice.

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+var app = builder.Build();
+
+// configure the HTTP request pipeline
+
+app.MapCarter();
+
+app.Run();
